7e704f4e678742c9ad96da58ef0f2f67f34105df
Pierlu <pierluigiviti@gmail.com>
2019-02-18 17:35:39 +0100

    Upd: QueryFilter improvement

--- test/query_filter.js
+++ test/query_filter.js
@@ -5,6 +5,7 @@ const query = require('../index').query
 let qf = new query.QueryFilter()
     .filter('filtro1', 'val1')
     .filter('filtro2', 'val2')
+    .filters({'filtro3':'val3', 'filtro4':'val4', 'filtro5':'val5'})
     .include('res1', 'res2')
     .include(['res3', 'res4'])
     .fields('res1', 'field1.1', 'field1.2')
@@ -14,6 +15,7 @@ let qf = new query.QueryFilter()
     .sort('sort3', false)
     .param('key1', 'val1')
     .param('key2', 'val2')
+    .params({'key3':'val3', 'key4':'val4', 'key5':'val5'})
     .page(2, 10)
 
 
