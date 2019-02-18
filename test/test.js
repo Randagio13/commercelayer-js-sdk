a7c2269b63e94127eabe14e9b63c6dd042647b08
Pierlu <pierluigiviti@gmail.com>
2019-02-18 15:13:30 +0100

    Upd: Version 1.3.0

--- test/test.js
+++ test/test.js
@@ -11,7 +11,8 @@ const inspect_enabled = true;
 
 const cfg = {
     client_id: '351020e9c84f2076755083f08bfe8e47365a76395db1059c3219c37abff86534',
-    market_id: '185'
+    market_id: '185',
+    base_url : "https://static-commerce.commercelayer.io",
 }
 
 module.exports.config = cfg;
