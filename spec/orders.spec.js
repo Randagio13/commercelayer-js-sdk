254b2dd943a55721168e50f91c831643b5ed308c
Pierlu <pierluigiviti@gmail.com>
2019-02-22 13:58:46 +0100

    Upd: update and create with filter param

--- spec/orders.spec.js
+++ spec/orders.spec.js
@@ -1,27 +1,51 @@
 const commercelayer = require('../index')
-const test = require = require('../test/test')
+const permissions = require('./support/permissions')
+const config = require('./support/config')
+const data = require('./support/data')
 
 
 describe("Orders", function() {
 
     beforeAll(function() {
-        commercelayer.settings.debug = false;
-        commercelayer.initialize(test.config);
-        // commercelayer.settings.console = false;
+        commercelayer.initialize(config);
     });
   
-    it("create", function() {
-        return commercelayer.createOrder(new commercelayer.model.Order())
-            .then(response => {
-                expect(response.get('id')).not.toBeNull();                
-            })
-    });
+    if (permissions.Orders.includes('create'))
+        it("create", function() {
+            return commercelayer.createOrder(new commercelayer.model.Order())
+                .then(response => {
+                    expect(response.get('id')).not.toBeNull();                
+                })
+        });
+    else console.log('Test Orders.create skipped due to lack of required permissions on the resource')
 
-    it("retrieve", function() {
-        return commercelayer.retrieveOrder(9839)
-            .then(response => {
-                expect(response.get('id')).toBe('9839')
-            })
-    });
+    if (permissions.Orders.includes('retrieve'))
+        it("retrieve", function() {
+            return commercelayer.retrieveOrder(data.Orders.retrieve.id)
+                .then(response => {
+                    expect(response.get('id')).toBe(data.Orders.retrieve.id)
+                })
+        });
+    else console.log('Test Orders.retrieve skipped due to lack of required permissions on the resource')
+
+    if (permissions.Orders.includes('update'))
+        it("update", function() {
+            return commercelayer.updateOrder(data.Orders.update.id, new commercelayer.model.Order().setFields(data.Orders.update.fields))
+                .then(response => {
+                    Object.keys(data.Orders.update.fields).forEach(field => {
+                        expect(response.get(field).toBe(data.Orders.update.fields[field]))
+                    })
+                })
+        });
+    else console.log('Test Orders.update skipped due to lack of required permissions on the resource')
+
+    if (permissions.Orders.includes('list'))
+        it("list", function() {
+            return commercelayer.listOrders()
+                .then(response => {
+                    expect(response.get(['id']).length).toBeGreaterThan(0)
+                })
+        });
+    else console.log('Test Orders.list skipped due to lack of required permissions on the resource')
 
   });
---
+++ spec/support/data.js
@@ -0,0 +1,17 @@
+
+const Orders = {
+    retrieve : {
+        id : '9839'
+    },
+    update : {
+        id : '9839',
+        fields : {
+            guest : 'pippo',
+            customer_email : 'pippo@server.com'
+        }
+    }
+}
+
+module.exports = {
+    Orders
+}
