348382f2cf18b0c228762916012b3ca92b7823d0
Pierlu <pierluigiviti@gmail.com>
2019-03-01 15:37:58 +0100

    Fix: test credentials

--- spec/support/config.js
+++ spec/support/config.js
@@ -1,10 +1,10 @@
 
 
 const cfg = {
-    client_id: '82acede30b1083bd9c83ed8d15b81efe94afbcbaf75a4979eb0968c55a8bd1ad',
-	client_secret: 'd020c7b44f994b54412e43dd792519770dbc5b84eb4b37a3569ee0cd49b6be16',
+    client_id: null,
+	client_secret: null,
     market_id: null,
-    base_url : "https://static-commerce.commercelayer.io",
+    base_url : null
 }
 
 module.exports = cfg;
