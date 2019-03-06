a6bfd39cce530910a5060cce068f9e58dcb338b4
Pierlu <pierluigiviti@gmail.com>
2019-03-06 16:13:47 +0100

    New: QueryExpression implementation

--- test/query_filter.js
+++ test/query_filter.js
@@ -1,8 +1,10 @@
 
 const query = require('../index').query
+const queryx = require('../lib/queryx')
 
 
-let qf = new query.QueryFilter()
+// let qf = new query.QueryFilter()
+let qf = new queryx.QueryExpression()
     .filter('filtro1', 'val1')
     .filter('filtro2', 'val2')
     .filters({'filtro3':'val3', 'filtro4':'val4', 'filtro5':'val5'})
@@ -17,6 +19,8 @@ let qf = new query.QueryFilter()
     .param('key2', 'val2')
     .params({'key3':'val3', 'key4':'val4', 'key5':'val5'})
     .page(2, 10)
+    .predicate('eq', 'PIPPO', 'first_name', 'last_name')
+    ._gt(50, 'age')
 
 
 console.log('build()')
