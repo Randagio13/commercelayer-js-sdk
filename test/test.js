b6039eaed6c4f9d3fb23bd63f71cecf25ca8eb1e
Pierlu <pierluigiviti@gmail.com>
2019-03-08 16:37:49 +0100

    Fix: test configuration

--- test/test.js
+++ test/test.js
@@ -10,10 +10,10 @@ const log_enabled = commercelayer.settings.debug && commercelayer.settings.conso
 const inspect_enabled = true;
 
 const cfg = {
-    // client_id: '351020e9c84f2076755083f08bfe8e47365a76395db1059c3219c37abff86534',
-    // market_id: '185',
-    client_id: '82acede30b1083bd9c83ed8d15b81efe94afbcbaf75a4979eb0968c55a8bd1ad',
-	client_secret: 'd020c7b44f994b54412e43dd792519770dbc5b84eb4b37a3569ee0cd49b6be16',
+    // client_id: '',
+    // market_id: '',
+    client_id: '',
+	client_secret: '',
     market_id: null,
     base_url : "https://static-commerce.commercelayer.io",
 }
