5a64d667f48e53764f90e6642fd3587dd54555e3
Pierlu <pierluigiviti@gmail.com>
2019-03-18 17:36:18 +0100

    Fix: delete return error

--- test/line_items.js
+++ test/line_items.js
@@ -11,14 +11,19 @@ commercelayer.initialize(test.config);
 // test.execute(commercelayer.retrieveLineItem(35169));
 
 
-test.separator('createLineItem()');
-let line_item = new commercelayer.model.LineItem();
+// test.separator('createLineItem()');
+// let line_item = new commercelayer.model.LineItem();
 
-line_item.name = 'Prodotto';
-line_item.order = new commercelayer.model.Order(9839);
-line_item.item = new commercelayer.model.Sku(31472);
+// line_item.name = 'Prodotto';
+// line_item.order = new commercelayer.model.Order(9839);
+
+// line_item.item = new commercelayer.model.Sku(31472);
+// line_item.quantity=1;
+
+// test.execute(commercelayer.createLineItem(line_item));
 
 
-test.execute(commercelayer.createLineItem(line_item));
+test.separator('deleteLineItem()');
+test.execute(commercelayer.deleteLineItem(44148))
 
 
--- test/test.js
+++ test/test.js
@@ -4,21 +4,22 @@ const commercelayer = require('../index')
 
 commercelayer.settings.debug = true;
 commercelayer.settings.console = true;
-commercelayer.settings.trace = true;
+commercelayer.settings.trace = false;
 
-const log_enabled = commercelayer.settings.debug && commercelayer.settings.console;
-const inspect_enabled = true;
+const log_enabled = true;
+const inspect_enabled = false;
 
 const cfg = {
-    // client_id: '',
-    // market_id: '',
-    client_id: '',
-	client_secret: '',
+    authentication : commercelayer.auth.ClientCredentials(
+        '82acede30b1083bd9c83ed8d15b81efe94afbcbaf75a4979eb0968c55a8bd1ad',
+        'd020c7b44f994b54412e43dd792519770dbc5b84eb4b37a3569ee0cd49b6be16'
+    ),
     market_id: null,
     base_url : "https://static-commerce.commercelayer.io",
 }
 
 module.exports.config = cfg;
+
 
 module.exports.separator = (text) => {
     let line = '********************';
